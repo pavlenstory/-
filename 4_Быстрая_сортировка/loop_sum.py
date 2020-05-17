0b8d0d957169e7edb97b9227410b5cc24dce6a7a
pavlenstory <55143655+pavlenstory@users.noreply.github.com>
2020-05-17 10:35:14 +0300

    feat: add book lessons

---
+++ 4_Быстрая_сортировка/loop_sum.py
@@ -0,0 +1,9 @@
+def sum(arr):
+    total =     for x in arr:
+        total += x
+    return total
+
+print(sum([1, 2, 3, 4]))
+
+
+0
---
+++ 5_Хеш_таблицы/hash_tables.py
@@ -0,0 +1,4 @@
+book = {"apple": 0.67, "milk": 1.49, "avocado": 1.49}
+print(book)
+
+print(book["avocado"])
